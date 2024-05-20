18f02e47892961e8c1a9bb5c608fca88e2d71a74
Plinio <brasil.plinio@gmail.com>
2024-05-20 09:51:35 -0300

    update tiposvariaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // classe String representa texto na aplicação
+
+        int numero = 5;
+
+        System.out.println("numero");
+               
     }
 }
