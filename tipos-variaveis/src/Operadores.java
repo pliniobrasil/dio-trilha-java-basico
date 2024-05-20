371b846d90fe646de3872cb8a04f61d472c1ddd7
Plinio <brasil.plinio@gmail.com>
2024-05-20 10:10:58 -0300

    update operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -3,5 +3,12 @@ public class Operadores {
     String nomeCompleto = "LINGUAGUEM" + "JAVA";
 
     System.out.println(nomeCompleto);
+
+    String concatencao = "1";
+
+    concatencao = "1"+(1+1+1);
+
+    System.out.println(concatencao);
+
    } 
 }
